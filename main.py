d187d3557fc7b194b743b608e7f936fc59ac7cf9
mwealth0 <moseswealth549@gmail.com>
2026-05-22 11:36:01 -0700

    messages goes here

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 name = "moses" # string variable
-age = 20
-height = 6.2
-is_online = True
-#camelCase:  (e.g., firstName, calculateTotalAmount).
-#PascalCase: (e.g., FirstName, CustomerRecord).
-#snake_case:  (e.g., first_name, user_account_id).
-#kebab-case:  (e.g., first-name, button-style).
-#(MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global#
+age = 20 # this is an intergal variable
+height = 6.2 # this is a floot veriable
+is_online = True # this is a blooant
+# camelCase:  (e.g., firstName, calculateTotalAmount).
+# PascalCase: (e.g., FirstName, CustomerRecord).
+# snake_case:  (e.g., first_name, user_account_id).
+# kebab-case:  (e.g., first-name, button-style).
+# (MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global#
