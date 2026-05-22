1f2453b8d1e2e069d54bb10f85677f3944090cc4
mwealth0 <moseswealth549@gmail.com>
2026-05-22 11:07:07 -0700

    for the class

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("my first python from cle") 
+name = "moses" # string variable
+age = 20
+height = 6.2
+is_online = True
+#camelCase:  (e.g., firstName, calculateTotalAmount).
+#PascalCase: (e.g., FirstName, CustomerRecord).
+#snake_case:  (e.g., first_name, user_account_id).
+#kebab-case:  (e.g., first-name, button-style).
+#(MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global#
